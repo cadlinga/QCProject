2e77cc28c6a2584f623d23d96346898563cfa786
Conner <conneradlington@gmail.com>
2024-01-23 15:58:33 +0000

    Add pyright config and remove useless boilerplate

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -5,7 +5,8 @@ from utils.addition import addition
 
 class TestAddition(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(addition(1, 3), 4)
+        self.assertEqual(addition(1, 2), 3)
+        self.assertEqual(addition(2, 3), 5)
 
 
 # if __name__ == "__main__":
--- utils/addition.py
+++ utils/addition.py
@@ -1,2 +1,3 @@
 def addition(a: int, b: int) -> int:
     return a + b
+    # return 1
